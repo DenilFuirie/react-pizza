be7cab227360c77b9b9ad04a2125afd5361c55b4
Daniil <rusm9snik2013@yandex.ru>
2021-02-13 18:59:54 +0300

    switch case

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,8 +38,7 @@ const Home = ( ) => {
             <div className="content__items">
                 {isLoaded
                     ? items.map((obj) => <PizzaBlock isLoading={true} key={obj.id} {...obj}/>)
-                    : Array(12).fill(<LoadingBlock/>)
-                }
+                    : Array(12).fill(0).map((_, index) => <LoadingBlock key={index}/>)}
 
 
             </div>
