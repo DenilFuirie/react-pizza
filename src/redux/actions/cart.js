3c3ec84f2ef3ce3621b5298deb9a404e62f38cc4
Daniil <rusm9snik2013@yandex.ru>
2021-02-26 23:16:17 +0300

    Done

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -1,6 +1,6 @@
 export const addPizzaToCart = (pizzaObj) => ({
     type: 'ADD_PIZZA_CART',
-    payload: pizzaObj
+    payload: pizzaObj,
 });
 
 export const clearCart = () => ({
@@ -9,5 +9,15 @@ export const clearCart = () => ({
 
 export const removeCartItem = (id) => ({
     type: 'REMOVE_CART_ITEM',
+    payload: id,
+});
+
+export const plusCartItem = (id) => ({
+    type: 'PLUS_CART_ITEM',
+    payload: id,
+});
+
+export const minusCartItem = (id) => ({
+    type: 'MINUS_CART_ITEM',
     payload: id,
 });
